bab53b0f09cb9b9911037704b3a877f18b3a5fd5
Albino-Hamambocegi <pamirbirlikk@gmail.com>
2026-03-10 21:15:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+def generate_password(Length: int):
+
+    password = ""
+
+    for i in range(length):
+
+        password += random.choice(characters)
+
+    return password
+def save_passwords(account: str, password: str):
+    with open("passwords.txt", "a", encoding="utf8")as file:  
+        file.write(f"{account} hesabı şifren: {password}\n")
+
+account_name=input("Hangi Hesabın Için Şifre Istiyorsun?\n")
+length = int(input("Şifren Kaç Karakterli Olsun?\n"))
+password = generate_password(length)
+save_passwords(account_name,password)
+print(password)
